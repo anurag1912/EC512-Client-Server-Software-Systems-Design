ebeb1e41368a17a52bf8c6007bb14c758494ab74
Anurag Mashruwala <anuragm@bu.edu>
2016-03-26 17:34:52 -0400

    Added files via upload

---
+++ ConversionsusingSQL/Modify.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Modify.aspx.cs" Inherits="Modify" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Modify Existing Unit-Factor Database</title>
+</head>
+<body>
+    <form id="form1" runat="server" method="post" action="Modify.aspx">
+    <div style="background-color: #C0C0C0; border-style: solid; border-width: 1px; margin: auto; width: 800px; padding: 10px">
+    
+         <h1 style="text-align: center">Update Database</h1>
+         <h2>Add units/factors</h2>
+         <br />
+      <br />
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Conversions]" InsertCommand="INSERT INTO Conversions(Units, Factor) VALUES (@newunit, @convFactor)" DeleteCommand="DELETE FROM Conversions WHERE (Id = @Id)">
+            <DeleteParameters>
+                <asp:ControlParameter ControlID="output3" Name="Id" PropertyName="SelectedValue" />
+            </DeleteParameters>
+            <InsertParameters>
+                <asp:ControlParameter ControlID="newunit" Name="newunit" PropertyName="Text" />
+                <asp:ControlParameter ControlID="convFactor" Name="convFactor" PropertyName="Text" />
+            </InsertParameters>
+         </asp:SqlDataSource>
+       
+      <asp:Label ID="Label1" runat="server" Text="Enter unit:"></asp:Label> &nbsp 
+          <asp:textbox ID="newunit" runat="server" style="width:147px"></asp:textbox> &nbsp &nbsp
+      <asp:Label ID="Label2" runat="server" Text="Enter factor:"></asp:Label> &nbsp
+          <asp:textbox ID="convFactor" runat="server" style="width:147px"></asp:textbox>
+      <br />
+        
+                
+
+    <br />
+        <br />
+      <asp:Button ID="add" runat="server" Text="Add" OnClick="add_Click" />
+      
+      <h2>Delete units/factors</h2>
+      <br />
+      <br />
+       <asp:DropDownList ID="output3" runat="server" DataSourceID="SqlDataSource1" DataTextField="Units" DataValueField="Id"></asp:DropDownList> &nbsp &nbsp
+     &nbsp &nbsp
+      <asp:Button ID="delete" runat="server" Text="Delete" OnClick="delete_Click" /> &nbsp &nbsp
+      <asp:Button ID="show" runat="server" Text="Show Factor" OnClick="show_Click" /> &nbsp &nbsp
+      <asp:TextBox ID="sfactor" runat="server" Readonly="true"></asp:TextBox> &nbsp &nbsp
+        <asp:Button ID="return" runat="server" Text="Return to Convert" OnClick="return_Click" />
+          <br />
+      <br />
+      <br />
+        <asp:Label ID="comment1" runat="server" style="text-decoration:solid"></asp:Label>
+
+
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ ConversionsusingSQL/Modify.aspx.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+public partial class Modify : System.Web.UI.Page
+{
+   
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void add_Click(object sender, EventArgs e)
+    {
+
+        
+        
+        
+        
+        string s = null;
+        bool exc0 = false, exc1 = false, exc2 = false, exc3 = false;
+        double val;
+        
+        DataView dv =
+                  (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+        if (dv == null)
+        {
+            //handle error
+        }
+        //The select query has returned the desired row
+        DataRow row;
+        for (int i = 0; i < dv.Count; i++)
+        {
+            row = dv.Table.Rows[i];
+            s = (string)row["Units"];
+            if(String.Compare(newunit.Text,s)==0)
+            {   
+                comment1.Text = "Unit already exists: enter new unit";
+                newunit.Text = "";
+                convFactor.Text = "";
+                exc3=true;
+                break;
+            }
+        }
+        
+        
+
+        if (exc3 == false)
+        {
+            try
+            {
+                val = Convert.ToDouble(convFactor.Text);
+                if (val == 0)
+                {
+                    comment1.Text = "Enter non-zero value for factor";
+                    newunit.Text = "";
+                    convFactor.Text = "";
+                    exc0 = true;
+                }
+            }
+            catch (FormatException)
+            {
+
+                comment1.Text = "Please enter a decimal value";
+                newunit.Text = "";
+                convFactor.Text = "";
+                exc1 = true;
+            }
+
+            catch (OverflowException)
+            {
+                comment1.Text = "Bad input: data exceeded bounds";
+                newunit.Text = "";
+                convFactor.Text = "";
+                exc2 = true;
+            }
+            if (exc0 == false && exc1 == false && exc2 == false)
+            {bool exc4=false;
+                String s1 = newunit.Text;
+                int i=0;
+                char[] input = s1.ToCharArray();
+                for( i=0;i<s1.Length;i++)
+                {
+                    if (input[i] != ' ')
+                        break;
+                }
+                if (i == s1.Length)
+                {
+                    comment1.Text = "Unit should have a name";
+                    newunit.Text="";
+                    convFactor.Text="";
+                    exc4 = true;
+                }
+                if (newunit.Text != "" & convFactor.Text != "")
+                {
+                    if (exc4 == false)
+                    {
+                        SqlDataSource1.Insert();
+                        newunit.Text = "";
+                        convFactor.Text = "";
+                        comment1.Text = "Factor added successfully!";
+                    }
+                }
+
+            }
+        }
+    
+    
+    
+    
+    
+    }
+    protected void delete_Click(object sender, EventArgs e)
+    {
+        sfactor.Text = "";
+        DataView dv =
+                      (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+        if (dv.Count == 0)
+        {
+            comment1.Text = "Error: No items to delete!";
+        }
+        else
+        {
+            comment1.Text = "Successfully deleted 1 item!";
+            if (output3.SelectedItem != null)
+                SqlDataSource1.Delete();
+
+        }
+    }
+    protected void show_Click(object sender, EventArgs e)
+    {
+        String s = null;
+        DataView dv =
+                    (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+        if (dv == null)
+        {
+            comment1.Text = "No units/factors found in database!";
+        }
+       // comment1.Text = output3.Text;
+       
+        DataRow row;
+        for (int i = 0; i < dv.Count;i++)
+        { 
+            row = dv.Table.Rows[i];
+            s = Convert.ToString(row["Id"]);
+            if(String.Compare(output3.Text,s)==0)
+            {s = Convert.ToString(row["Factor"]);
+            sfactor.Text = s;
+            break;
+            }
+        }
+
+
+
+            }
+    protected void return_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Default.aspx");
+    }
+}
+
+    
+  
